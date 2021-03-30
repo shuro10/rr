aec54ecc71c7b61c5a8de66530221a77e19305f6
kent7k <kent.ki720@gmail.com>
2021-03-30 12:11:27 +0900

    feat: route53

--- terraform/rds_sg.tf
+++ terraform/rds_sg.tf
@@ -5,14 +5,6 @@ resource "aws_security_group" "cs-rds-sg" {
   vpc_id      = aws_vpc.cs-vpc.id
 }
 
-/* security group for ALB */
-resource "aws_security_group" "cs-alb-sg" {
-  description = "ALB security group for cs"
-  name        = "cs-alb-sg"
-  vpc_id      = aws_vpc.cs-vpc.id
-}
-
-
 /* security group for RDS */
 resource "aws_security_group_rule" "cs-rds-sg-rule1" {
   description       = "cs-rds-sg-rule1"

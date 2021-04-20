d07f79f2d00d02169b4c019c63dcc2551fd89492
kent7k <kent.ki720@gmail.com>
2021-04-20 15:45:45 +0900

    CircleCI Integration

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    # origins 'http://localhost:3000'
-
+    origins ENV['API_DOMAIN'] || 'http://localhost:3000'
+    
     resource '*',
             headers: :any,
             expose: %w[access-token uid client token-type expiry],

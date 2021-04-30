7ca5c0f413cad3e0805cafbeee44b8d7d69b6f0a
kent7k <kent.ki720@gmail.com>
2021-04-30 15:14:18 +0900

    refactor: added environment variables

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,7 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 /* ==========Weather========== */
-require('dotenv').config();
-const { WEATHER_API_KEY } = process.env;
+require('dotenv').config()
+const { WEATHER_API_KEY } = process.env
+const { RAILS_GUEST_ADDRESS } = process.env
+const { RAILS_GUEST_KEY } = process.env
+const { BASE_URL } = process.env
+
 /* ==================== */
 
 export default {
@@ -11,7 +15,10 @@ export default {
    ** Headers of the page
    */
   env: {
-    WEATHER_API_KEY
+    WEATHER_API_KEY,
+    RAILS_GUEST_ADDRESS,
+    RAILS_GUEST_KEY,
+    BASE_URL,
   },
   head: {
     // titleTemplate: '%s - ' + process.env.npm_package_name,
@@ -22,10 +29,10 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
@@ -34,67 +41,66 @@ export default {
   /*
    ** Global CSS
    */
-  css: [
-    
-  ],
+  css: [],
   /*
    ** Plugins to load before mounting the App
    */
   plugins: [
     { src: '~/plugins/axios.js', ssr: false },
-    { src: "~/plugins/dayjs", ssr: false },
-    { src: "~/plugins/localStorage.js", ssr: false },
+    { src: '~/plugins/dayjs', ssr: false },
+    { src: '~/plugins/localStorage.js', ssr: false },
   ],
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    '@nuxtjs/vuetify',
-    '@nuxt/typescript-build', 
-  ],
+  buildModules: ['@nuxtjs/vuetify', '@nuxt/typescript-build'],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // '@nuxtjs/proxy',
     '@nuxtjs/axios',
-    "nuxt-webfontloader",
-    "nuxt-i18n",
-    "nuxt-client-init-module",
+    'nuxt-webfontloader',
+    'nuxt-i18n',
+    'nuxt-client-init-module',
   ],
   // proxy: {
-  //   '/api': { 
+  //   '/api': {
   //     target: 'https://openweathermap.org',
   //     pathRewrite: {
   //       '^/api': '/api'
   //     }
   //   },
-  // '/api': { 
+  // '/api': {
   //   target: 'http://localhost:5000',
   //   // pathRewrite: {
   //   //   '^/api': '/api'
   //   // }
   // },
   axios: {
-    baseURL: 'http://localhost:5000',
-   },
-   auth: {
-     redirect: {
-       login: '/users/login', 
-       logout: '/',
-       callback: false,
-       home: '/'
-     },
-     strategies: {
-       local: {
-         endpoints: {
-           login: { url: '/api/v1/auth/sign_in', method: 'post', propertyName: 'token' },
-           logout: false,
-           user: false
-         }
-       }
-     }  
-   },
+    baseURL: process.env.BASE_URL
+  },
+  auth: {
+    redirect: {
+      login: '/users/login',
+      logout: '/',
+      callback: false,
+      home: '/',
+    },
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: '/api/v1/auth/sign_in',
+            method: 'post',
+            propertyName: 'token',
+          },
+          logout: false,
+          user: false,
+        },
+      },
+    },
+  },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
@@ -111,10 +117,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
   /*
    ** Build configuration
@@ -122,10 +128,10 @@ export default {
   build: {
     /*
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {},
   },
 
-/* ================ */
+  /* ================ */
 
   //   router: {
   //   middleware: ['auth']

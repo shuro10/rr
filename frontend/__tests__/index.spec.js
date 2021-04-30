bc5f645a00c4f5d543a6d7d53329eef39cb7b76a
kent7k <kent.ki720@gmail.com>
2021-04-30 12:31:57 +0900

    refactor: deleted the first-test folder from the frontend.

--- frontend/__tests__/index.spec.js
+++
@@ -1,9 +0,0 @@
-import Index from '../pages/index.vue'
-
-describe('pages/index.vue', () => {
-  it('has a default value', () => {
-    const defaultData = Index.data()
-    // subTitle の初期値をテストする
-    expect(defaultData.subTitle).toBe('Zenn is good service!!')
-  })
-})

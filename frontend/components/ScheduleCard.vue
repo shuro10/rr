f07b28b2cd3169a3dbdeee648081620497adcfae
kent7k <kent.ki720@gmail.com>
2021-05-03 15:21:27 +0900

    refactor: edited view

--- frontend/components/ScheduleCard.vue
+++ frontend/components/ScheduleCard.vue
@@ -17,26 +17,31 @@
             class="d-flex child-flex flex-wrap"
             cols="4"
           >
-            <v-card :elevation="10" class="secondary ma-3 rounded-card">
+            <v-card 
+            :elevation="15" 
+            dark
+            class="green secondary ma-3 rounded-card">
               <v-responsive :aspect-ratio="9 / 16">
                 <v-hover>
                   <template v-slot:default="{ hover }">
-                    <v-sheet dark flat>
+                    <v-sheet dark flat color="white">
                       <v-img
                         v-if="post.image.url"
                         contain
                         :src="post.image.url"
                         :aspect-ratio="1 / 1"
                         class="white--text align-end"
-                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                       
                       >
                         <!--!!!!!!!!!!!! :aspect-ratio="16/9" !!!!!!!!!!!!-->
                         <!-- <v-img v-else contain :src="defaultImage"> -->
 
                         <v-card-title>
-                          <strong class="display-2 font-weight-regular">{{
-                            post.name
-                          }}</strong>
+                          <strong class="display-2 font-weight-regular"
+                          gradient="to top, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                          >
+                          <!-- gradient="to top, rgba(0,0,0,.1), rgba(0,0,0,.5)" -->
+                          {{ post.name }}</strong>
                         </v-card-title>
 
                         <v-fade-transition>
@@ -60,6 +65,7 @@
                           fab
                           right
                           top
+                          @click="nice"
                         >
                           <v-icon>mdi-heart-off</v-icon>
                         </v-btn>
@@ -71,42 +77,25 @@
                           fab
                           right
                           top
+                          @click="nice"
                         >
                           <v-icon>mdi-heart</v-icon>
                         </v-btn>
+
                         <div>
                           <span class="font-weight-light grey--text title mb-2">
-                            {{ post.release }} [表示させたい場所] 〜
+                            {{ $dayjs(post.release).format('MM/DD') }} 
+                            ( {{ $dayjs(post.start_time).format('hh:mm') }}~{{ $dayjs(post.finish_time).format('hh:mm') }} )
+                            <br>@{{ post.place}}
                           </span>
                         </div>
-                        <div class="font-weight-light title mb-2">
-                          Our Vintage kitchen utensils delight.<br />
-                        </div>
                       </v-card-text>
-                    </v-sheet>
-                  </template>
-                </v-hover>
-
-                <v-card>
-                  <v-card-text>
-                    <div class="font-weight-bold ml-8 mb-2">
-                      メッセージ:
-                      <!-- <user-dialog-review :users="post.reviews" :title="'メッセージしたユーザー'" /> -->
-                      <br />
-                      気になる:
-                      <!-- <user-dialog :users="post.like_users" :title="'気になるユーザー'" /> -->
-                    </div>
-                  </v-card-text>
-
-                  <v-card-actions>
                     <v-avatar size="56" class="mt-1">
                       <img
                         alt="user"
                         :src="require(`@/assets/images/default-user.png`)"
                       />
                     </v-avatar>
-
-                    <v-spacer></v-spacer>
 
                     <v-menu transition="slide-y-reverse-transition">
                       <template v-slot:activator="{ on, attrs }">
@@ -115,20 +104,284 @@
                           class="ma-2"
                           v-bind="attrs"
                           v-on="on"
+                          outlined
                         >
                           <v-icon>{{ 'mdi-information-outline' }}</v-icon>
                         </v-btn>
                       </template>
-                      <v-list>
-                        <v-list-item v-for="n in 5" :key="n" link>
-                          <v-list-item-title
-                            v-text="'Item ' + n"
-                          ></v-list-item-title>
-                        </v-list-item>
-                      </v-list>
+
+<v-card  color="white" max-width="600" min-width="200">
+
+
+            <v-card 
+            :elevation="15" 
+            dark
+            class="green secondary ma-3 rounded-card">
+              <v-responsive :aspect-ratio="9 / 16">
+                <v-hover>
+                  <template v-slot:default="{ hover }">
+                    <v-sheet dark flat color="white">
+                      <v-img
+                        v-if="post.image.url"
+                        contain
+                        :src="post.image.url"
+                        :aspect-ratio="1 / 1"
+                        class="white--text align-end"
+                       
+                      >
+                        <!--!!!!!!!!!!!! :aspect-ratio="16/9" !!!!!!!!!!!!-->
+                        <!-- <v-img v-else contain :src="defaultImage"> -->
+
+                        <v-card-title>
+                          <strong class="display-2 font-weight-regular"
+                          gradient="to top, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                          >
+                          <!-- gradient="to top, rgba(0,0,0,.1), rgba(0,0,0,.5)" -->
+                          {{ post.name }}</strong>
+                        </v-card-title>
+
+                        <v-fade-transition>
+                          <v-overlay v-if="hover" absolute color="#036358">
+                            <v-btn
+                              large
+                              :to="{ path: `/post/${post.id}` }"
+                              @click="pagelink(post.id)"
+                              >参加ページ</v-btn
+                            >
+                          </v-overlay>
+                        </v-fade-transition>
+                      </v-img>
+
+                      <v-card-text class="pt-6" style="position: relative;">
+                        <v-btn
+                          v-if="like"
+                          absolute
+                          color="pink lighten-1"
+                          class="white--text"
+                          fab
+                          right
+                          top
+                          @click="nice"
+                        >
+                          <v-icon>mdi-heart-off</v-icon>
+                        </v-btn>
+                        <v-btn
+                          v-else
+                          absolute
+                          color="pink lighten-1"
+                          class="white--text"
+                          fab
+                          right
+                          top
+                          @click="nice"
+                        >
+                          <v-icon>mdi-heart</v-icon>
+                        </v-btn>
+                        <div>
+                          <span class="font-weight-light grey--text title mb-2">
+                            {{ $dayjs(post.release).format('MM/DD') }} 
+                            ( {{ $dayjs(post.start_time).format('hh:mm') }}~{{ $dayjs(post.finish_time).format('hh:mm') }} )
+                            <br>@{{ post.place}}
+                          </span>
+                        </div>
+                      </v-card-text>
+                    <v-avatar size="56" class="mt-1">
+                      <img
+                        alt="user"
+                        :src="require(`@/assets/images/default-user.png`)"
+                      />
+                    </v-avatar>
+<div align="right">
+                    <v-menu transition="slide-y-reverse-transition">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          color="secondary"
+                          class="ma-2"
+                          v-bind="attrs"
+                          v-on="on"
+                          outlined
+                        >
+                          <v-icon>{{ 'mdi-information-outline' }}</v-icon>
+                        </v-btn>
+                      </template>
+
+<v-card  color="white" max-width="600" min-width="200">
+
+  <v-card color="white" class="mx-auto">
+          <v-card-header class="overline mb-4">
+            詳細
+          </v-card-header>
+                      <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+              {{ post.details }}
+            </div>
+  
+
+          </v-card-text>
+          
+</v-card>
+<v-container class=ma-10 width="300px">
+    <v-simple-table height="300px">
+      <template v-slot:default>
+        <tbody>
+          <tr>
+          </tr>
+          <tr>
+            <td>日付</td>
+            <td>{{ post.name }}</td>
+          </tr>
+          <tr>
+            <td>時間</td>
+            <td>{{ post.name }}</td>
+          </tr>
+          
+        </tbody>
+      </template>
+    </v-simple-table>
+</v-container>
+</v-card>
+
                     </v-menu>
-                  </v-card-actions>
-                </v-card>
+
+</div>
+                    </v-sheet>
+                  </template>
+                </v-hover>
+
+                  <v-card-text>
+                    <div class="font-weight-bold">
+                      <br />
+                      <v-icon>mdi-heart</v-icon> ( <user-dialog :users="post.like_users" :title="'気になるユーザー'" /> )
+                      <v-icon>mdi-run</v-icon> (<user-dialog :users="post.like_users" :title="'気になるユーザー'" /> / {{ post.member }} )
+
+
+                <v-sheet
+                  class="green lighten-2 mb-4"
+                  align="center"
+                  justify="center"
+                >
+                  <v-btn
+                    v-if="like"
+                    class="mx-5"
+                    color="red white--text font-weight-bold"
+                    outlined
+                    @click="nice"
+                  >
+                    <v-icon>mdi-heart-off</v-icon>
+                    「気になる」から外す
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    class="mx-5"
+                    color="green white--text font-weight-bold"
+                    @click="nice"
+                  >
+                    <v-icon class="mr-1">mdi-heart</v-icon>
+                    気になる！
+                  </v-btn>
+                </v-sheet>
+
+                  <v-sheet class="green lighten-2 mb-4">
+                    <v-timeline align-top dense>
+                      <v-timeline-item color="white lighten-3" fill-dot small>
+                        <v-row class="pt-1">
+                          <v-col cols="3">
+                            <strong>集合: {{ post.start_time }}</strong>
+                          </v-col>
+                          <v-col>
+                            <strong
+                              >場所: {{ post.place }} / 募集メンバー:
+                              {{ post.member }}人</strong
+                            >
+                            <div class="caption mb-2">
+                              予算: {{ post.price }}円/人
+                            </div>
+                            <v-avatar>
+                              <v-img
+                                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
+                              ></v-img>
+                            </v-avatar>
+                            <v-avatar>
+                              <v-img
+                                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+                              ></v-img>
+                            </v-avatar>
+                            <v-avatar>
+                              <v-img
+                                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale"
+                              ></v-img>
+                            </v-avatar>
+                          </v-col>
+                        </v-row>
+                      </v-timeline-item>
+                      <v-timeline-item fill-dot small>
+                        <v-row class="pt-1">
+                          <v-col cols="3">
+                            <strong>終了: {{ post.finish_time }}</strong>
+                          </v-col>
+                        </v-row>
+                      </v-timeline-item>
+                    </v-timeline>
+                  </v-sheet>
+
+
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr>
+          </tr>
+          <tr>
+            <td>日付</td>
+            <td>{{ post.name }}</td>
+          </tr>
+          <tr>
+            <td>時間</td>
+            <td>{{ post.start_time }} 〜 {{ post.start_time }}</td>
+          </tr>
+          
+        </tbody>
+      </template>
+    </v-simple-table>
+                    </div>
+                  </v-card-text>
+
+              </v-responsive>
+            </v-card>
+            
+
+
+  <v-card color="white" class="mx-auto">
+          <v-card-header class="overline mb-4">
+            詳細
+          </v-card-header>
+                      <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+              {{ post.details }}
+            </div>
+  
+
+          </v-card-text>
+          
+</v-card>
+
+
+</v-card>
+
+                    </v-menu>
+
+
+                    </v-sheet>
+                  </template>
+                </v-hover>
+
+                  <v-card-text>
+                    <div class="font-weight-bold ml-8 mb-2">
+                      <br />
+                      <v-icon>mdi-heart</v-icon> ( <user-dialog :users="post.like_users" :title="'気になるユーザー'" /> )
+                      <v-icon>mdi-run</v-icon> (<user-dialog :users="post.like_users" :title="'気になるユーザー'" /> / {{ post.member }} )
+                    </div>
+                  </v-card-text>
+
               </v-responsive>
             </v-card>
           </v-col>
@@ -141,8 +394,8 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 // import userPostList from '~/components/infoUser/UserPostList.vue'
-// import userDialog from '~/components/infoUser/UserDialog.vue'
-// import userDialogReview from '~/components/infoUser/UserDialogReview.vue'
+import userDialog from '~/components/infoUser/UserDialog.vue'
+import userDialogReview from '~/components/infoUser/UserDialogReview.vue'
 
 export default {
   // props: {
@@ -153,8 +406,8 @@ export default {
   // },
   components: {
     // userPostList,
-    // userDialog,
-    // userDialogReview
+    userDialog,
+    userDialogReview
   },
   data() {
     return {
@@ -164,8 +417,6 @@ export default {
       like: false,
       defaultImage: require(`@/assets/images/default.png`),
       posts: [],
-      start_time: '',
-      finish_time: '',
     }
   },
   computed: {
@@ -194,6 +445,7 @@ export default {
     pagelink(link) {
       this.$router.push({ path: `/post/${link}` })
     },
+
   },
 }
 </script>
@@ -202,4 +454,5 @@ export default {
 .rounded-card {
   border-radius: 20px;
 }
+
 </style>

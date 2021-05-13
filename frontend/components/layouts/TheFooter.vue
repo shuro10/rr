414a8e14045dab68316c37222487a71c8cfb4747
kent7k <kent.ki720@gmail.com>
2021-05-13 12:37:34 +0900

    add: lodash in config

--- frontend/components/layouts/TheFooter.vue
+++ frontend/components/layouts/TheFooter.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-footer class="flex justify-center py-2" absolute>
-    <div>&copy; {{ new Date().getFullYear() }}<span class="ml-2">RR</span></div>
+  <v-footer class="elevation-10 footertitle flex justify-center mt-5" color="#BADEC3" absolute>
+    <div>&copy; {{ new Date().getFullYear() }}<span class="ml-2">MeetWithKids</span></div>
   </v-footer>
 </template>
+
+<style scoped>
+.footertitle {
+  color: white;
+  font-size: 18px;
+  font-family: 'Gill Sans', sans-serif;
+  /* https://developer.mozilla.org/en-US/docs/Web/CSS/font-family */
+}
+</style>

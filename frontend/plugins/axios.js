d610cc93a2b734caf2471386394706355cd80e13
kent7k <kent.ki720@gmail.com>
2021-05-26 18:52:49 +0900

    refactor: for production

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -2,12 +2,12 @@ export default function({ $axios }) {
   // $axios.setToken("access_token")
   $axios.onRequest((config) => {
     console.log('-------- start /plugins/axios.js------------')
-    config.headers.client = window.localStorage.getItem('client')
-    config.headers['access-token'] = window.localStorage.getItem('access-token')
-    config.headers.uid = window.localStorage.getItem('uid')
-    config.headers['token-type'] = window.localStorage.getItem('token-type')
-    config.headers.expiry = window.localStorage.getItem('expiry')
-    config.headers['content-type'] = 'application/json'
+    // config.headers.client = window.localStorage.getItem('client')
+    // config.headers['access-token'] = window.localStorage.getItem('access-token')
+    // config.headers.uid = window.localStorage.getItem('uid')
+    // config.headers['token-type'] = window.localStorage.getItem('token-type')
+    // config.headers.expiry = window.localStorage.getItem('expiry')
+    // config.headers['content-type'] = 'application/json'
     console.log(config)
   })
 

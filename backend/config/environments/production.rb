46f2532078b387bd91ae22ea7979bfefbd123070
kent7k <kent.ki720@gmail.com>
2021-05-27 18:21:27 +0900

    refactor: for production

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -73,11 +73,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  # if ENV['RAILS_LOG_TO_STDOUT'].present?
+  #   logger           = ActiveSupport::Logger.new($stdout)
+  #   logger.formatter = config.log_formatter
+  #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # end
 
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new

e01d75a4aae01abb0a26b83dddf838325e260186
kent7k <kent.ki720@gmail.com>
2021-05-28 12:04:58 +0900

    refactor: for production

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -58,32 +58,32 @@ resource "aws_acm_certificate" "meetwithkids-frontend-acm" {
 }
 
 /* Backend: Definition of SSL証明書 */
-resource "aws_acm_certificate" "meetwithkids-backend-acm" {
-  domain_name               = aws_route53_record.meetwithkids-host-zone-record.name
-  subject_alternative_names = []
-  validation_method         = "DNS"
+# resource "aws_acm_certificate" "meetwithkids-backend-acm" {
+#   domain_name               = aws_route53_record.meetwithkids-host-zone-record.name
+#   subject_alternative_names = []
+#   validation_method         = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "Backend: rinrei720.com"
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+#   tags = {
+#     "Name" = "Backend: rinrei720.com"
+#   }
+# }
 
 /* Definition of Record for ValidationOfSSL証明書 */
-resource "aws_route53_record" "meetwithkids-certificate" {
-  name    = tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_type
-  records = [tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_value]
-  zone_id = aws_route53_zone.meetwithkids-host-zone.id
-  ttl     = 60
-}
+# resource "aws_route53_record" "meetwithkids-certificate" {
+#   name    = tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_name
+#   type    = tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_type
+#   records = [tolist(aws_acm_certificate.meetwithkids-backend-acm.domain_validation_options)[0].resource_record_value]
+#   zone_id = aws_route53_zone.meetwithkids-host-zone.id
+#   ttl     = 60
+# }
 
 /* Waiting for FinishOfValidation */
-resource "aws_acm_certificate_validation" "meetwithkids-backend-acm" {
-  certificate_arn         = aws_acm_certificate.meetwithkids-backend-acm.arn
-  validation_record_fqdns = [aws_route53_record.meetwithkids-certificate.fqdn]
-}
+# resource "aws_acm_certificate_validation" "meetwithkids-backend-acm" {
+#   certificate_arn         = aws_acm_certificate.meetwithkids-backend-acm.arn
+#   validation_record_fqdns = [aws_route53_record.meetwithkids-certificate.fqdn]
+# }
 
 /* ========================================== */

7a859134f0056bfe0eac3cf0bb958d7e41f488cf
kent7k <kent.ki720@gmail.com>
2021-05-28 11:34:50 +0900

    refactor: for production

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -81,7 +81,7 @@ export default {
   axios: {
     baseURL:
       process.env.NODE_ENV === 'production'
-        ? 'https://rinrei720.com'
+        ? 'http://rinrei720.com'
         : 'http://localhost:5000',
     // baseURL: process.env.NODE_ENV === "production" ? "https://meetwithkids.org" : "http://localhost:5000"
     // baseURL: "https://meetwithkids.org",

4674e731607cca89c689900109c9d7d6052a84c3
kent7k <kent.ki720@gmail.com>
2021-05-30 13:30:54 +0900

    add: Jest test

---
+++ frontend/test/FloatingButton.spec.js
@@ -0,0 +1,9 @@
+import PostCreateComponent from '~/components/layouts/PostCreateComponent.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe("FormSubmitter", () => {
+  it("フォームを送信する", () => {
+    const wrapper = shallowMount(PostCreateComponent)
+    // 開発中
+  })
+})

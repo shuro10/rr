21e0aca70f74c7087389b5e1c3c83edba7c8fa9f
kent7k <kent.ki720@gmail.com>
2021-05-31 14:51:25 +0900

    refactor: db:seed for production

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -10,4 +10,8 @@ resource "aws_cloudwatch_log_group" "meetwithkids-ecs-db-migrate" {
   name              = "/ecs/db-migrate"
   retention_in_days = 180
 }
+resource "aws_cloudwatch_log_group" "meetwithkids-ecs-db-seed" {
+  name              = "/ecs/db-seed"
+  retention_in_days = 180
+}
 

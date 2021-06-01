0dedecce0b3788212e2022321e1455d9ad739183
kent7k <kent.ki720@gmail.com>
2021-06-01 10:20:35 +0900

    refactor: for production

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -5,11 +5,6 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.cache_storage = :fog
-    # === add ===
-    config.fog_provider = 'fog/aws'
-    config.asset_host = 'https://meetwithkids-s3-bucket-for-images.s3.amazonaws.com'
-    config.fog_public = true
-    # === add ===
     config.fog_directory = 'meetwithkids-s3-bucket-for-images'
     config.fog_credentials = {
       provider: 'AWS',

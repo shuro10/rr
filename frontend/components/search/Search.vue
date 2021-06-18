86a0491d9e3858cf9ae4f0f0240d45c70d6d4e96
kent7k <kent.ki720@gmail.com>
2021-06-18 16:33:14 +0900

    refactor: help

--- frontend/components/search/Search.vue
+++ frontend/components/search/Search.vue
@@ -20,14 +20,44 @@
         prepend-inner-icon="mdi-magnify"
       />
 
-        <v-btn
-          text
-          class="ml-4 mr-2 font-weight-bold"
-          color="white"
-          @click.stop="loginDialog(true)"
+<v-row justify="space-around">
+      <v-col cols="auto">
+        <v-dialog
+          transition="dialog-bottom-transition"
+          max-width="600"
         >
-          ヘルプ
-        </v-btn>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              text
+              v-bind="attrs"
+              v-on="on"
+              class="ml-4 mr-2 font-weight-bold"
+              color="white"
+              @click.stop="loginDialog(true)"
+            >ヘルプ</v-btn>
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-card-text>
+                <div class="text-h2 pa-12">
+                  
+                  
+                  <schedule-card-info-init />
+
+
+                  </div>
+              </v-card-text>
+              <v-card-actions class="justify-end">
+                <v-btn
+                  text
+                  @click="dialog.value = false"
+                >Close</v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-col>
+    </v-row>
 
       <template v-if="!isLogin">
         <v-btn
@@ -82,6 +112,9 @@ import theModalSignUp from '~/components/layouts/TheModalSignUp.vue'
 import theModalLogin from '~/components/layouts/TheModalLogin.vue'
 import buttonGuestLogin from '~/components/layouts/ButtonGuestLogin.vue'
 import dialogComponent from '~/components/layouts/DialogComponent.vue'
+import scheduleCardInfoInit from '~/components/infoPost/ScheduleCardInfoInit.vue'
+
+
 
 /* import searchPost from '~/components/search/SearchPost.vue' */
 import ScheduleCard2 from '~/components/search/ScheduleCard2.vue'
@@ -95,6 +128,7 @@ export default {
     theModalLogin,
     dialogComponent,
     buttonGuestLogin,
+    scheduleCardInfoInit,
     /* TheHeader */
 
     /* searchPost, */
